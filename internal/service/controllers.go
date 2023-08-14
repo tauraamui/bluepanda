806a5ec86cfd17363ed0dba0352fc9e09d12875b
tauraamui <adampstringer@protonmail.com>
2023-08-14 12:59:07 +0100

    feat: switch from using JSON marshaller to manual encoding

--- internal/service/controllers.go
+++ internal/service/controllers.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"math"
 	"net/http"
 	"reflect"
 	"strings"
@@ -14,20 +16,25 @@ import (
 	"github.com/tauraamui/redpanda/internal/logging"
 )
 
+type typedEntry struct {
+	t reflect.Type
+	e kvs.Entry
+}
+
 func handleFetch(log logging.Logger, store kvs.KVDB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		ttype := c.Params("type")
 		uuidx := c.Params("uuid")
 
-		data := map[string]any{}
+		data := map[string]string{}
 		json.Unmarshal(c.Body(), &data)
 
-		blankEntries := convertToBlankEntries(ttype, resolveOwnerID(uuidx), uint32(0), data)
+		blankEntries := convertToBlankTypesEntries(ttype, resolveOwnerID(uuidx), uint32(0), data)
 
 		dest := []rawData{}
 		for _, ent := range blankEntries {
 			// iterate over all stored values for this entry
-			prefix := ent.PrefixKey()
+			prefix := ent.e.PrefixKey()
 			if err := store.View(func(txn *badger.Txn) error {
 				it := txn.NewIterator(badger.DefaultIteratorOptions)
 				defer it.Close()
@@ -36,7 +43,7 @@ func handleFetch(log logging.Logger, store kvs.KVDB) fiber.Handler {
 				for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
 					item := it.Item()
 					if err := item.Value(func(val []byte) error {
-						ent.Data = val
+						ent.e.Data = val
 						return nil
 					}); err != nil {
 						return err
@@ -46,7 +53,13 @@ func handleFetch(log logging.Logger, store kvs.KVDB) fiber.Handler {
 						dest = append(dest, rawData{})
 					}
 
-					dest[destinationindex][ent.ColumnName] = ent.Data
+					v := reflect.New(ent.t).Interface()
+					fmt.Printf("LEN: %d\n", len(ent.e.Data))
+					if err := convertFromBytes(ent.e.Data, v); err != nil {
+						return err
+					}
+					fmt.Printf("V: %+v\n", v)
+					dest[destinationindex][ent.e.ColumnName] = v
 
 					destinationindex++
 				}
@@ -100,6 +113,31 @@ func loadItemDataIntoEntry(ent *kvs.Entry, fn func(func(val []byte) error) error
 	})
 }
 
+func convertToBlankTypesEntries(tableName string, ownerUUID kvs.UUID, rowID uint32, data map[string]string) []typedEntry {
+	entries := []typedEntry{}
+	for k, v := range data {
+		e := kvs.Entry{
+			TableName:  tableName,
+			ColumnName: strings.ToLower(k),
+			OwnerUUID:  ownerUUID,
+			RowID:      rowID,
+		}
+
+		entries = append(entries, typedEntry{t: nameToTypeOf(v), e: e})
+	}
+	return entries
+}
+
+func nameToTypeOf(name string) reflect.Type {
+	switch name {
+	case "string":
+		return reflect.TypeOf("")
+	case "int":
+		return reflect.TypeOf(int(0))
+	}
+	return nil
+}
+
 func convertToBlankEntries(tableName string, ownerUUID kvs.UUID, rowID uint32, data map[string]any) []kvs.Entry {
 	return convertToEntries(tableName, ownerUUID, rowID, data, false)
 }
@@ -130,38 +168,129 @@ func convertToEntries(tableName string, ownerUUID kvs.UUID, rowID uint32, data m
 }
 
 func convertToBytes(i interface{}) ([]byte, error) {
-	// Check the type of the interface.
 	switch v := i.(type) {
 	case []byte:
-		// Return the input as a []byte if it is already a []byte.
 		return v, nil
 	case string:
-		// Convert the string to a []byte and return it.
 		return []byte(v), nil
+	case int:
+		buf := make([]byte, 4)
+		binary.BigEndian.PutUint32(buf, uint32(v))
+		return buf, nil
+	case int32:
+		buf := make([]byte, 4)
+		binary.BigEndian.PutUint32(buf, uint32(v))
+		return buf, nil
+	case int64:
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, uint64(v))
+		return buf, nil
+	case uint:
+		buf := make([]byte, 4)
+		binary.BigEndian.PutUint32(buf, uint32(v))
+		return buf, nil
+	case uint32:
+		buf := make([]byte, 4)
+		binary.BigEndian.PutUint32(buf, v)
+		return buf, nil
+	case uint64:
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, v)
+		return buf, nil
+	case float32:
+		bits := math.Float32bits(v)
+		buf := make([]byte, 4)
+		binary.BigEndian.PutUint32(buf, bits)
+		return buf, nil
+	case float64:
+		bits := math.Float64bits(v)
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, bits)
+		return buf, nil
+	case bool:
+		if v {
+			return []byte{1}, nil
+		} else {
+			return []byte{0}, nil
+		}
 	default:
-		// Use json.Marshal to convert the interface to a []byte.
-		return json.Marshal(v)
+		return nil, fmt.Errorf("unsupported type")
 	}
 }
 
 func convertFromBytes(data []byte, i interface{}) error {
-	// Check that the destination argument is a pointer.
 	if reflect.TypeOf(i).Kind() != reflect.Ptr {
 		return fmt.Errorf("destination must be a pointer")
 	}
 
-	// Check the type of the interface.
 	switch v := i.(type) {
 	case *[]byte:
-		// Set the value of the interface to the []byte if it is a pointer to a []byte.
 		*v = data
 		return nil
 	case *string:
-		// Convert the []byte to a string and set the value of the interface to the string.
 		*v = string(data)
 		return nil
+	case *int:
+		if len(data) == 2 {
+			data = append(data, 0x0000)
+			data = append(data, 0x0000)
+		}
+		if len(data) < 4 {
+			return fmt.Errorf("insufficient data for int")
+		}
+		*v = int(binary.BigEndian.Uint32(data))
+		return nil
+	case *int32:
+		if len(data) < 4 {
+			return fmt.Errorf("insufficient data for int32")
+		}
+		*v = int32(binary.BigEndian.Uint32(data))
+		return nil
+	case *int64:
+		if len(data) < 8 {
+			return fmt.Errorf("insufficient data for int64")
+		}
+		*v = int64(binary.BigEndian.Uint64(data))
+		return nil
+	case *uint:
+		if len(data) < 4 {
+			return fmt.Errorf("insufficient data for uint")
+		}
+		*v = uint(binary.BigEndian.Uint32(data))
+		return nil
+	case *uint32:
+		if len(data) < 4 {
+			return fmt.Errorf("insufficient data for uint32")
+		}
+		*v = binary.BigEndian.Uint32(data)
+		return nil
+	case *uint64:
+		if len(data) < 8 {
+			return fmt.Errorf("insufficient data for uint64")
+		}
+		*v = binary.BigEndian.Uint64(data)
+		return nil
+	case *float32:
+		if len(data) < 4 {
+			return fmt.Errorf("insufficient data for float32")
+		}
+		bits := binary.BigEndian.Uint32(data)
+		*v = math.Float32frombits(bits)
+		return nil
+	case *float64:
+		if len(data) < 8 {
+			return fmt.Errorf("insufficient data for float64")
+		}
+		bits := binary.BigEndian.Uint64(data)
+		*v = math.Float64frombits(bits)
+		return nil
+	case *bool:
+		if len(data) < 1 {
+			return fmt.Errorf("insufficient data for bool")
+		}
+		*v = data[0] != 0
+		return nil
 	default:
-		// Use json.Unmarshal to convert the []byte to the interface.
-		return json.Unmarshal(data, v)
+		return fmt.Errorf("unsupported type")
 	}
 }
